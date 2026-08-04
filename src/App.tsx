e54a7704aedbe4b5ea40dce7158bbd97a28b409e
Phuong Dong <catmusic435@gmail.com>
2026-08-04 19:16:37 +0700

    fix: off debug

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import { FloatingMenu } from './components/ui/FloatingMenu';
 import { RightToolbar } from './components/ui/RightToolbar';
 import { useUIStore } from './store/useUIStore';
 import { useTransition, animated } from '@react-spring/web';
-import { Leva } from 'leva';
-import { DebugPanel } from './components/panorama/DebugPanel';
+// import { Leva } from 'leva';
 const PanoramaViewer = React.lazy(() => import('./components/panorama/PanoramaViewer').then(m => ({ default: m.PanoramaViewer })));
 const OverviewModal = React.lazy(() => import('./components/modals/OverviewModal').then(module => ({ default: module.OverviewModal })));
 const FloorPlanModal = React.lazy(() => import('./components/modals/FloorPlanModal').then(module => ({ default: module.FloorPlanModal })));
@@ -61,10 +60,7 @@ function App() {
       </Suspense>
       
       {/* Hide Leva globally */}
-      <Leva hidden />
-      
-      {/* Debug Panel */}
-      <DebugPanel />
+      {/* <Leva hidden /> */}
     </div>
   );
 }
